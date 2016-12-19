0d3f8933b639d7950eda8c9252945995667b1422
robgmerrill <robgmerrill@gmail.com>
2016-12-18 20:25:51 -0800

    added component

---
+++ mirage/config.js
@@ -0,0 +1,33 @@
+export default function() {
+  this.namespace = '/api';
+
+  this.get('/dogparks', function() {
+    return {
+      data: [{
+        type: 'dogparks',
+        id: 'off-leash',
+        attributes: {
+          title: 'Marymoore Dog Park',
+          city: 'Seattle',
+          image: 'http://www.blackcatwhitecatphotography.com/pics/dogs/marymoor/MarymoorDogs04.jpg'
+        }
+      }, {
+        type: 'dogparks',
+        id: 'on-leash',
+        attributes: {
+          title: 'Seward Park',
+          city: 'Seattle',
+          image: 'http://www.activelynorthwest.com/wp-content/uploads/2013/05/Week20_SewardPark.jpg'
+        }
+      }, {
+        type: 'dogparks',
+        id: 'on-leash',
+        attributes: {
+          title: 'Greenlake Park',
+          city: 'Seattle',
+          image: 'http://activelifestylesnw.com/wp-content/uploads/2015/06/greenlakePath2.jpg'
+        }
+      }]
+    };
+  });
+}
