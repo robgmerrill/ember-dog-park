1fbc19fc183ceadbcbdceef01a5a0a4b095a9a1f
robgmerrill <robgmerrill@gmail.com>
2016-12-18 19:20:37 -0800

    added model data

--- app/routes/dogparks.js
+++ app/routes/dogparks.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
+let dogparks = [{
+  id: 'off-leash',
+  title: 'Marymoore Dog Park',
+  city: 'Seattle',
+  image: 'http://www.blackcatwhitecatphotography.com/pics/dogs/marymoor/MarymoorDogs04.jpg'
+}, {
+  id: 'on-leash',
+  title: 'Seward Park',
+  city: 'Seattle',
+  image: 'http://www.activelynorthwest.com/wp-content/uploads/2013/05/Week20_SewardPark.jpg'
+}, {
+  id: 'on-leash',
+  title: 'Greenlake Park',
+  city: 'Seattle',
+  image: 'http://activelifestylesnw.com/wp-content/uploads/2015/06/greenlakePath2.jpg'
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return dogparks;
+  }
 });
